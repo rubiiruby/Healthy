fb755b219bbecf607f85feaa292061b31c9d0c09
rubiiruby <ruby.penporn@gmail.com>
2018-08-20 16:35:46 +0700

    20/8/18-2

--- app/src/main/java/com/example/a59070123/healthy/BMIFragment.java
+++ app/src/main/java/com/example/a59070123/healthy/BMIFragment.java
@@ -4,11 +4,13 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by LAB203_03 on 20/8/2561.
@@ -36,7 +38,11 @@ public class BMIFragment extends Fragment {
                 int kg_int = Integer.parseInt(kg_str);
 
                 if (cm_str.isEmpty() || kg_str.isEmpty()) {
-
+                    Toast.makeText(getActivity(),
+                            "กรุณาระบุข้อมูลให้ครบถ้วน",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    Log.d("USER", "FIELD NAME IS EMPTY");
                 }
                 else {
                     float bmi = kg_int / ((cm_int/100)*(cm_int/100));
