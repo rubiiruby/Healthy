2261e7fb2aae2305ca6217dd8441ba2fd323b5f3
Ruby <ruby.penporn@gmail.com>
2018-09-10 16:49:51 +0700

    10/9/18 in class part 2 test test

--- app/src/main/java/com/example/a59070123/healthy/RegisterFragment.java
+++ app/src/main/java/com/example/a59070123/healthy/RegisterFragment.java
@@ -14,12 +14,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.a59070123.healthy.weight.WeightFormFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by LAB203_03 on 20/8/2561.
  */
 
 public class RegisterFragment extends Fragment {
+
+    private FirebaseAuth fbAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,61 +38,16 @@ public class RegisterFragment extends Fragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        initRegisterBTN();
-        initBackBTN();
         super.onActivityCreated(savedInstanceState);
+
+        fbAuth = FirebaseAuth.getInstance();
+
+        initBackBTN();
+        registerNewUser();
+
     }
 
-    void initRegisterBTN(){
-        Button btn_regis = (Button) getView().findViewById(R.id.regis_btn_regisnew);
-        btn_regis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText user = (EditText) getView().findViewById(R.id.regis_userid);
-                EditText pass = (EditText) getView().findViewById(R.id.regis_pass);
-                EditText re_pass = (EditText) getView().findViewById(R.id.regis_repass);
 
-                String user_str = user.getText().toString();
-                String pass_str = pass.getText().toString();
-                String re_pass_str = re_pass.getText().toString();
-
-                if (user_str.isEmpty() || pass_str.isEmpty() || re_pass_str.isEmpty()){
-                    Toast.makeText(getActivity(),
-                            "กรุณาระบุกรอกข้อมูลให้ครบถ้วน",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                    Log.d("USER", "FIELD NAME IS EMPTY");
-                }
-
-                else if(user_str.length() < 6){
-                    Toast.makeText(getActivity(),
-                            "กรุณาระบุกรอกPasswordให้มากว่า6ตัวอักษร",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                    Log.d("USER", "pass < 6");
-                }
-
-                else if(pass_str != re_pass_str){
-                    Toast.makeText(getActivity(),
-                            "กรุณาระบุกรอกPasswordให้ตรงกัน",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                    Log.d("USER", "pass != repass");
-                }
-
-
-                else {
-                    getActivity().getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.main_view, new BMIFragment())
-                            .addToBackStack(null)
-                            .commit();
-                    Log.d("USER", "GOTO BMI");
-                }
-
-            }
-        });
-    }
     void initBackBTN(){
         TextView btn_regis_back = (TextView) getView().findViewById(R.id.regis_btn_back);
         btn_regis_back.setOnClickListener(new View.OnClickListener() {
@@ -93,9 +56,80 @@ public class RegisterFragment extends Fragment {
                 Log.d("Weight", "Click ADD FORM");
                 getActivity().getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.main_view, new WeightFormFragment()).addToBackStack(null)
+                        .replace(R.id.main_view, new LoginFragment()).addToBackStack(null)
                         .commit();
             }
         });
     }
+    void registerNewUser(){
+        Button btn_regis = (Button) getView().findViewById(R.id.regis_btn_regisnew);
+        btn_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = (EditText) getView().findViewById(R.id.regis_email);
+                EditText pass = (EditText) getView().findViewById(R.id.regis_pass);
+                EditText re_pass = (EditText) getView().findViewById(R.id.regis_repass);
+
+                String email_str = email.getText().toString();
+                String pass_str = pass.getText().toString();
+                String re_pass_str = re_pass.getText().toString();
+
+                if (email_str.isEmpty() || pass_str.isEmpty() || re_pass_str.isEmpty()) {
+                    Toast.makeText(getActivity(),
+                            "กรุณากรอกข้อมูลให้ครบถ้วน",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    Log.d("USER", "FIELD NAME IS EMPTY");
+                } else if (pass_str.length() < 6) {
+                    Toast.makeText(getActivity(),
+                            "กรุณากรอกPasswordให้มากกว่า6ตัวอักษร",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    Log.d("USER", "pass < 6");
+                } else if (!pass_str.equals(re_pass_str)) {
+                    Toast.makeText(getActivity(),
+                            "กรุณากรอกPasswordให้ตรงกัน",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    Log.d("USER", "pass != repass");
+                }else if(pass_str.equals(re_pass_str)){
+                    fbAuth.createUserWithEmailAndPassword(email_str, pass_str).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            sendVerifiedEmail(authResult.getUser());
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), "ERROR = "+e.getMessage(),Toast.LENGTH_SHORT);
+                        }
+                    });
+
+                }
+            }
+        });
+
+    }
+
+    private void sendVerifiedEmail(FirebaseUser user){
+        user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new LoginFragment())
+                        .commit();
+                Log.d("USER", "GOTO LOGIN");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+
+
+
 }
